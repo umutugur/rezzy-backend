32d947ac363f0fe2c4749413d0e03a9ca934bbfb
Umut Uğur <umutugur@Umut-MacBook-Air.local>
2025-10-13 16:41:18 +0300

    Apple Login2

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,7 +2,7 @@
 import { Router } from "express";
 import { auth } from "../middlewares/auth.js";
 import multer from "multer";
-import { getMe, updateMe, uploadAvatar, changePassword } from "../controllers/user.controller.js";
+import { getMe, updateMe, uploadAvatar, changePassword, deleteMe } from "../controllers/user.controller.js";
 
 const r = Router();
 const upload = multer(); // memory storage
@@ -11,5 +11,5 @@ r.get("/me", auth(), getMe);
 r.patch("/me", auth(), updateMe);
 r.post("/me/avatar", auth(), upload.single("file"), uploadAvatar);
 r.post("/change-password", auth(), changePassword);
-
+r.delete("/me",auth(),deleteMe);
 export default r;
