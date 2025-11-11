6efd119a9dc866035f4eebb6209025485f11c551
Umut Uğur <umutugur@Umut-MacBook-Air.local>
2025-11-12 01:37:19 +0300

    Mongo Index

--- src/config/db.js
+++ src/config/db.js
@@ -2,6 +2,11 @@ import mongoose from "mongoose";
 
 export async function connectDB(uri) {
   mongoose.set("strictQuery", true);
-  await mongoose.connect(uri);
-  console.log("✅ MongoDB connected");
+
+  await mongoose.connect(uri, {
+    autoIndex: true, // ⬅️ Prod’da da index’leri oluştur
+    maxPoolSize: 10, // bağlantı optimizasyonu
+  });
+
+  console.log("✅ MongoDB connected (autoIndex ON)");
 }
