36033c867d77e57fe00aee91b0f0a2ad02af2d6e
Umut Uğur <umutugur@Umut-MacBook-Air.local>
2025-11-12 20:49:09 +0300

    Header remove

--- src/app.js
+++ src/app.js
@@ -31,7 +31,7 @@ app.use(cors({
   origin: (process.env.CORS_ORIGIN || "").split(",").map(s=>s.trim()).filter(Boolean),
   credentials: true,
   methods: ["GET","POST","PUT","PATCH","DELETE","OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],   // ⬅️ önemli
+  allowedHeaders: ["*"],   // ⬅️ önemli
   exposedHeaders: ["Content-Disposition"],
 }));
 app.use(express.json({ limit: "12mb" }));
