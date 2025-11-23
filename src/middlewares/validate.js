67870fd88be0844f0b93df6e8bbeffa61de70dc8
Umut Uğur <umutugur@MacBookAir.home>
2025-11-23 16:59:19 +0300

    validate error2

--- src/middlewares/validate.js
+++ src/middlewares/validate.js
@@ -1,23 +1,7 @@
-// src/middlewares/validate.js
 import Joi from "joi";
-
-export const validate = (schema, location = "body") => (req, res, next) => {
-  const data = req[location];
-
-  const { error, value } = schema.validate(data, {
-    abortEarly: false,
-    allowUnknown: false,
-    stripUnknown: true,
-    convert: true,
-  });
-
-  if (error) {
-    return next({
-      status: 400,
-      message: error.details.map((d) => d.message).join(", "),
-    });
-  }
-
-  req[location] = value;
+export const validate = (schema) => (req,res,next)=>{
+  const data = { body:req.body, params:req.params, query:req.query };
+  const { error } = schema.validate(data, { abortEarly:false, allowUnknown:true });
+  if (error) return next({ status:400, message: error.details.map(d=>d.message).join(", ") });
   next();
 };
