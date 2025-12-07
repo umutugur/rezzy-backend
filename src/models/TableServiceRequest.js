3d17ac2f9d97a82bc60be806fceae609580c2b24
Umut Uğur <umutugur@Umut-MacBook-Air.local>
2025-12-08 00:31:31 +0300

    Kitchen 8th try

--- src/models/TableServiceRequest.js
+++ src/models/TableServiceRequest.js
@@ -6,7 +6,7 @@ const schema = new mongoose.Schema(
     restaurantId: { type: mongoose.Schema.Types.ObjectId, ref: "Restaurant", required: true },
     tableId: { type: String },
     sessionId: { type: mongoose.Schema.Types.ObjectId, ref: "OrderSession" },
-    type: { type: String, enum: ["waiter", "bill"], required: true },
+    type: { type: String, enum: ["waiter", "bill","order_ready"], required: true },
     status: { type: String, enum: ["open", "handled"], default: "open" },
   },
   { timestamps: true }
