157c5f6be0586bd8cc164e3aa5769d7ac106aa46
Umut Uğur <umutugur@Umut-MacBook-Air.local>
2025-12-11 02:38:54 +0300

    AllowManagerOrAdmin for all Routesv2

--- src/routes/report.routes.js
+++ src/routes/report.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { auth } from "../middlewares/auth.js";
-import { allow } from "../middlewares/roles.js";
+import { allow, allowLocationManagerOrAdmin} from "../middlewares/roles.js";
 import { validate } from "../middlewares/validate.js";
 import { restaurantKpisSchema } from "../validators/report.schema.js";
 import { restaurantKpis } from "../controllers/report.controller.js";
